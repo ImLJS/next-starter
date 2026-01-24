c1d2b01f32e50ded78c74955663051c6ec166466
ImLJS <109210868+ImLJS@users.noreply.github.com>
2026-01-24 11:42:57 +0530

    chore(home): update home style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 const Home = () => {
 	return (
-		<div>
-			<h1>Welcome to Next Starter</h1>
-			<p>
+		<div className="flex min-h-svh flex-col items-center justify-center space-y-5 text-center">
+			<h1 className="font-black text-5xl">Welcome to Next Starter</h1>
+			<p className="font-medium text-xl">
 				This is a starter template for Next.js with TypeScript, Drizzle ORM, and
 				Better Auth.
 			</p>
