00907d935d682fd3ec9f7e271862d803f03a0c59
ImLJS <109210868+ImLJS@users.noreply.github.com>
2026-01-24 11:30:56 +0530

    chore(fix): update schema path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "drizzle-kit";
 import { env } from "@/env";
 
 export default {
-	schema: "./src/server/db/schema.ts",
+	schema: "./src/lib/db/schemas/index.ts",
 	dialect: "postgresql",
 	dbCredentials: {
 		url: env.DATABASE_URL,
